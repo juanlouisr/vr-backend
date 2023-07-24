259b665adb869fba4abc27425d7b8f216b53fa62
juanrombetasik <juan.rombetasik@tiket.com>
2023-07-25 04:30:25 +0700

    chore: cleanup

--- src/main/java/com/juan/itb/vrbackend/VrBackendApplication.java
+++ src/main/java/com/juan/itb/vrbackend/VrBackendApplication.java
@@ -1,7 +1,14 @@
 package com.juan.itb.vrbackend;
 
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 public class VrBackendApplication {
@@ -10,14 +17,15 @@ public class VrBackendApplication {
 		SpringApplication.run(VrBackendApplication.class, args);
 	}
 
-//	@Bean
-//	ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
-//		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-//		initializer.setConnectionFactory(connectionFactory);
-//		ResourceDatabasePopulator resource =
-//				new ResourceDatabasePopulator(new ClassPathResource("schema.mysql"));
-//		initializer.setDatabasePopulator(resource);
-//		return initializer;
-//	}
+	@Bean
+	@Profile({"default"})
+	ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+		initializer.setConnectionFactory(connectionFactory);
+		ResourceDatabasePopulator resource =
+				new ResourceDatabasePopulator(new ClassPathResource("schema.mysql"));
+		initializer.setDatabasePopulator(resource);
+		return initializer;
+	}
 
 }
