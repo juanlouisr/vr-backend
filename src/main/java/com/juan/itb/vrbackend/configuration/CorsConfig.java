7d4b34c58c60ac14764899201e95e5a6b36ecd2a
juanrombetasik <juan.rombetasik@tiket.com>
2023-07-25 04:20:28 +0700

    feat: remove cors configurer

--- src/main/java/com/juan/itb/vrbackend/configuration/CorsConfig.java
+++ src/main/java/com/juan/itb/vrbackend/configuration/CorsConfig.java
@@ -1,24 +1,18 @@
 package com.juan.itb.vrbackend.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class CorsConfig {
-
-  @Bean
-  public CorsWebFilter corsFilter() {
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-
-    config.addAllowedOrigin("*"); // Allow requests from any origin
-    config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, etc.)
-    config.addAllowedHeader("*"); // Allow all headers
-
-    source.registerCorsConfiguration("/**", config);
-    return new CorsWebFilter(source);
-  }
-}
+//@Configuration
+//public class CorsConfig {
+//
+//  @Bean
+//  public CorsWebFilter corsFilter() {
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    CorsConfiguration config = new CorsConfiguration();
+//
+//    config.addAllowedOrigin("*"); // Allow requests from any origin
+//    config.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, PUT, etc.)
+//    config.addAllowedHeader("*"); // Allow all headers
+//
+//    source.registerCorsConfiguration("/**", config);
+//    return new CorsWebFilter(source);
+//  }
+//}
